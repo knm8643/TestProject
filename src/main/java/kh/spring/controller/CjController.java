5d065ca4a05bf1dc04b545a07a47ea393759de41
knm8643 <knm8643@nate.com>
2022-01-11 16:03:06 +0900

    here_cj new

--- src/main/java/kh/spring/controller/CjController.java
+++ src/main/java/kh/spring/controller/CjController.java
@@ -3,5 +3,8 @@ package kh.spring.controller;
 public class CjController {
    public static void main(String[] args) {
 	   System.out.println("안녕하세요 테스트입니다.");
+	   
+	   
+	   // 이곳입니다~~~ 이곳을 새로이 수정했어요 (16:02) 이충재
    }
 }
